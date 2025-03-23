27e71ef05759741c90a2898b017a996ef38168ab
dioneandonli <irene.ilagan@student.ateneo.edu>
2025-03-23 15:13:33 +0800

    Update views.py
    added view_bottles and add_bottle

--- MP3/MyInventorySystem/MyInventoryApp/views.py
+++ MP3/MyInventorySystem/MyInventoryApp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from MyInventoryApp.models import Supplier  
+from MyInventoryApp.models import Supplier, WaterBottle
 from django.shortcuts import redirect
 
 def view_supplier(request):
@@ -7,5 +7,11 @@ def view_supplier(request):
     return render(request, "view_supplier.html", {"suppliers": suppliers})
 def home_redirect(request):
     return redirect("view_supplier")
+def view_bottles(request):
+    bottles = WaterBottle.objects.all()
+    return render(request, 'view_bottles.html', {'bottles': bottles})
+def add_bottle(request):
+    suppliers = Supplier.objects.all() 
+    return render(request, "add_bottle.html", {"suppliers": suppliers})
     
 
